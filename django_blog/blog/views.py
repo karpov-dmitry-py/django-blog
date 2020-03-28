6c157f5cb05176376f5881fda4ee3f3e405d260c
Dmitry Karpov <dem0schase@gmail.com>
2020-03-28 23:34:58 +0300

    home and about v2

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+
+    {
+        'author': 'Dmitry Karpov',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 28, 2020'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 27, 2020',
+    }
+
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {'posts': posts, 'title': 'Home page' }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    context = {'title': 'About page'}
+    return render(request, 'blog/about.html', context)
