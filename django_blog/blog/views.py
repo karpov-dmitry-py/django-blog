05bf81ebe27832d09f883af1be9fcf1befcc41bc
Dmitry Karpov <dem0schase@gmail.com>
2020-04-03 01:26:34 +0300

    posts model and view updated

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-
-    {
-        'author': 'Dmitry Karpov',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 28, 2020'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 27, 2020',
-    }
-
-]
+# posts = [
+#
+#     {
+#         'author': 'Dmitry Karpov',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'March 28, 2020'
+#     },
+#     {
+#         'author': 'John Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'March 27, 2020',
+#     }
+#
+# ]
 
 def home(request):
-    context = {'posts': posts, 'title': 'Home page' }
+    context = {'posts': Post.objects.all(), 'title': 'Home page' }
     return render(request, 'blog/home.html', context)
 
 def about(request):
