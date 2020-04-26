adbde7ddbc6ff0f217cc403f9abd1213c4d5d6ac
Dmitry Karpov <dem0schase@gmail.com>
2020-04-27 00:16:27 +0300

    user profile update, articles list author image display

--- django_blog/users/models.py
+++ django_blog/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +9,13 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
+    def save(self, *args, **kwargs):
+
+        max_size_wanted = 300
+        super().save(*args, **kwargs)
+
+        image = Image.open(self.image.path)
+        if image.height > max_size_wanted or image.width > max_size_wanted:
+            result_size = (max_size_wanted, max_size_wanted)
+            image.thumbnail(result_size)
+            image.save(self.image.path)
