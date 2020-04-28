86fc6af6828146c1385d94afbd0774a74ac2dfa5
Dmitry Karpov <dem0schase@gmail.com>
2020-04-29 01:03:39 +0300

    class-based-views for posts (list, detail, create, update, delete), login/author checks

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,26 +1,48 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-
-# posts = [
-#
-#     {
-#         'author': 'Dmitry Karpov',
-#         'title': 'Blog Post 1',
-#         'content': 'First post content',
-#         'date_posted': 'March 28, 2020'
-#     },
-#     {
-#         'author': 'John Doe',
-#         'title': 'Blog Post 2',
-#         'content': 'Second post content',
-#         'date_posted': 'March 27, 2020',
-#     }
-#
-# ]
 
 def home(request):
     context = {'posts': Post.objects.all(), 'title': 'Home page' }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 def about(request):
     context = {'title': 'About page'}
